c2f4ee6b953911ac0e8d78f1ee2201615d058a46
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-22 23:16:43 +0800

    fix(.github/actions/install/scripts/other/pch.sh): update architecture check in pch.sh script
    Update the architecture check in the pch.sh script to correctly check for "X64" instead of "X86" when the operating system is Linux. This ensures that the correct pre-commit hooks are downloaded and used for the specified architecture.

--- .github/actions/install/scripts/other/pch.sh
+++ .github/actions/install/scripts/other/pch.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [[ $RUNNER_ARCH == "X86" && $RUNNER_OS == "Linux" ]]; then
+if [[ $RUNNER_ARCH == "X64" && $RUNNER_OS == "Linux" ]]; then
   gh release download "dev" --output "$HOME/.local/bin/pch" --pattern "pch-x86_64-unknown-linux-gnu" --repo "liblaf/pre-commit-hooks"
   chmod +x "$HOME/.local/bin/pch"
 else
