a69b48544677910b8af05d61d2b00fc950a01164
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-22 23:13:53 +0800

    fix(install): use absolute path for sourcing other scripts
    Instead of using a relative path to source other scripts, this commit updates the script to use an absolute path for better reliability and consistency.

--- .github/actions/install/scripts/other.sh
+++ .github/actions/install/scripts/other.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
 for pkg in "$@"; do
-  bash "scripts/other/$pkg.sh"
+  bash "$script_dir/other/$pkg.sh"
 done
