a284412f8712457fd220477b43ad5947a297f541
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-25 13:16:51 +0800

    fix(common): remove unnecessary app parameter in setting GH_TOKEN secret
    Remove the unnecessary `--app` parameter in setting the `GH_TOKEN` secret for both the `actions` and `dependabot` apps in the `github.sh` script. The parameter is not required for setting the secret and can be safely removed to simplify the script.

--- common/github.sh
+++ common/github.sh
@@ -6,8 +6,7 @@ set -o pipefail
 repo=$1
 
 if [[ -n ${GH_TOKEN-} ]]; then
-  gh secret set GH_TOKEN --app actions --body "$GH_TOKEN" --repo "$repo"
-  gh secret set GH_TOKEN --app dependabot --body "$GH_TOKEN" --repo "$repo"
+  gh secret set GH_TOKEN --body "$GH_TOKEN" --repo "$repo"
 fi
 
 # https://docs.github.com/en/rest/actions/permissions#set-default-workflow-permissions-for-a-repository
