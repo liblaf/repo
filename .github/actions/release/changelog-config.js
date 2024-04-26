78ee9882de9b261e8b5fb7e9871a09fb1888fc57
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-26 14:21:42 +0800

    fix: update config file path in release action
    Update the path to the config file in the release action to ensure correct configuration is used. This change aligns the action with the required setup.

--- .github/actions/release/changelog-config.js
+++ .github/actions/release/changelog-config.js
@@ -1,3 +1,20 @@
 "use strict";
-const config = require("conventional-changelog-angular-emoji");
-module.exports = config;
+const config = require("conventional-changelog-conventionalcommits");
+
+// https://github.com/ellerbrock/conventional-changelog-angular-emoji
+// https://github.com/TriPSs/conventional-changelog-action/issues/176#issuecomment-1238331441
+module.exports = config({
+  types: [
+    { type: "feat", section: "✨ Features" },
+    { type: "fix", section: "🐛 Bug Fixes" },
+    { type: "docs", section: "📖 Documentation" },
+    { type: "style", section: "💎 Styles" },
+    { type: "refactor", section: "📦 Code Refactoring" },
+    { type: "perf", section: "🚀 Performance Improvements" },
+    { type: "test", section: "🚨 Tests" },
+    { type: "build", section: "👷 Build" },
+    { type: "ci", section: "💻 Continuous Integration" },
+    { type: "chore", section: "🎫 Chores" },
+    { type: "revert", section: "🔙 Reverts" },
+  ],
+});
