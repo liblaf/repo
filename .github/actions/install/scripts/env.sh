6cc7c6e1f0f51c6cc0e08ab1949cc7e60d72ece1
liblaf <30631553+liblaf@users.noreply.github.com>
2024-09-21 21:20:40 +0800

    fix: ensure pre-commit hooks cache is correctly named and path is created
    Ensures the cache step in the CI workflow is properly named and the necessary directory for pre-commit hooks is created to prevent failures during setup.

--- .github/actions/install/scripts/env.sh
+++ .github/actions/install/scripts/env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
+mkdir -p -v ~/.local/bin
 if [[ ! ":$PATH:" =~ ~/.local/bin: ]]; then
   case "$RUNNER_OS" in
     Linux | macOS) echo ~/.local/bin >> "$GITHUB_PATH" ;;
